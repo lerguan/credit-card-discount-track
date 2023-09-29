4087e67dd6aa4fd9b8e3f4f37e1910cb6933e4f1
Leroy G <liguan698@gmail.com>
2023-09-28 19:24:27 -0700

    install dependencies and packages

---
+++ server/migrations/versions/d08bd94901c5_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: d08bd94901c5
+Revises: 
+Create Date: 2023-09-28 19:20:55.255526
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd08bd94901c5'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
