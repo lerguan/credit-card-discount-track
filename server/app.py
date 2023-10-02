307530f5dd4cf2ae507123c8ffe903c54cc2a058
Leroy G <liguan698@gmail.com>
2023-10-02 16:07:23 -0700

    add login resource

--- server/app.py
+++ server/app.py
@@ -51,7 +51,20 @@ class CheckSession(Resource):
             return user.to_dict(), 200
         return {'error':'401 Unauthorized'}, 401
 
+class Login(Resource):
+    def post(self):
+        request_json = request.get_json()
+        username = request_json.get('username')
+        password = request_json.get('password')
 
+        user = User.query.filter(User.username==username).first()
+
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            
+        return {'error':'401 Unauthoerized'}, 401
 
 
 api.add_resource(Signup, '/signup', endpoint='signup')
