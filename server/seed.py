aff879d09a12192de1f2f58f80a5b985278abd02
Leroy G <liguan698@gmail.com>
2023-10-02 12:49:27 -0700

    create fake user records

--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,32 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
+        print('Deleting all records...')
+        User.query.delete()
+
         print("Starting seed...")
-        # Seed code goes here!
+
+        users=[]
+        usernames=[]
+
+        for i in range(10):
+            username = fake.email()
+            while username in usernames:
+                username = fake.email()
+            usernames.append(username)
+            user = User(username = username,
+                        image_url = fake.url(),
+                        )
+            user.password_hash = user.username + 'password'
+            users.append(user)
+
+    db.session.add_all(users)
+
+
+
+        
