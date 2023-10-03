b0368e8ba88d5835050bc2bfc4d4149e3a6a3aca
Leroy G <liguan698@gmail.com>
2023-10-02 21:33:44 -0700

    create signup form

---
+++ client/src/components/SignUpForm.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from "react";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const SignUpForm = () => {
+  const [users, setUsers] = useState([{}]);
+  const [refreshPage, setRefreshPage] = useState(false);
+
+  useEffect(() => {});
+};
