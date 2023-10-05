5e686910d5b2f03b60c16edc7edfab21ffff4912
Leroy G <liguan698@gmail.com>
2023-10-04 22:16:27 -0700

    remove loginform and signupform imports

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
-import SignUpForm from "./SignUpForm";
-import LoginForm from "./LoginForm";
+import Login from "../pages/Login";
 
 function App() {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    fetch("/checksession").then((resp) => {
+      if (resp.ok) {
+        resp.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return <Login onLogin={setUser} />;
+
   return (
     <>
-      <h1>Project Client</h1>
-      <div>
-        <SignUpForm />
-      </div>
-      <div>
-        <LoginForm />
-      </div>
+      {/* <NavBar user={user} setUser={setUser} /> */}
+      <main>
+        <Switch>
+          <Route path="/">
+            {/* <CreditCardList /> */}
+            credit card list
+          </Route>
+        </Switch>
+      </main>
     </>
   );
 }
