97e62c3c370e1bd46fea9266fb3bb1dd6582bcb9
Leroy G <liguan698@gmail.com>
2023-10-04 22:11:33 -0700

    create login/signup logic

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,3 +1,31 @@
 import { useState } from "react";
 import LoginForm from "../components/LoginForm";
 import SignUpForm from "../components/SignUpForm";
+
+const Login = ({ onLogin }) => {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <>
+      {showLogin ? (
+        <>
+          <LoginForm onLogin={onLogin} />
+          <p>
+            Don't have an account?
+            <button onClick={() => setShowLogin(false)}>Sign Up</button>
+          </p>
+        </>
+      ) : (
+        <>
+          <SignUpForm onLogin={onLogin} />
+          <p>
+            Already have an account?
+            <button onClick={() => setShowLogin(true)}>Login</button>
+          </p>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Login;
