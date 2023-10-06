94eab1d3e1d67e29b9df231f43d018676676eea7
Leroy G <liguan698@gmail.com>
2023-10-05 22:04:02 -0700

    seed db with seed.py

--- server/models.py
+++ server/models.py
@@ -13,7 +13,7 @@ class User(db.Model, SerializerMixin):
     email = db.Column(db.String, unique=True, nullable=False)
     _password_hash = db.Column(db.String)
 
-    credit_cards = db.relationship('Creditcard', backref='user')
+    credit_cards = db.relationship('CreditCard', back_populates='user')
     stores = association_proxy('credit_cards', 'store', creator=lambda sr:CreditCard(store=sr))
 
     @hybrid_property
@@ -30,33 +30,36 @@ class User(db.Model, SerializerMixin):
     
     def __repr__(self):
         return f'<User {self.email}>'
-
-class CreditCard(db.Model, SerializerMixin):
-    __tablename__ = 'credit_cards'
-
-    id = db.Column(db.Integer, primary_key=True)
-    card_name = db.Column(db.String, nullable=False)
-    
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
-
-    def __repr__(self):
-        return f'<CreditCard {self.card_name}>'
     
 class Store(db.Model, SerializerMixin):
     __tablename__ = 'stores'
+    serialize_rules= ('-cardit_cards.store',)
 
     id = db.Column(db.Integer, primary_key=True)
-    store_name = db.Column(db.String, nullable=False)
+    store_name = db.Column(db.String)
     discount = db.Column(db.String)
-    expire_date = db.Column(db.DateTime)
+    expire_date = db.Column(db.String)
 
-    credit_cards = db.relationship('CreditCard', backref='store')
+    credit_cards = db.relationship('CreditCard', back_populates='store')
     users = association_proxy('credit_cards', 'user', creator=lambda ur:CreditCard(ur))
 
     def __repr__(self):
         return f'<Store {self.store_name} has {self.discount} on {self.credit_cards} expires on {self.expire_date}> '
 
+class CreditCard(db.Model, SerializerMixin):
+    __tablename__ = 'credit_cards'
+
+    id = db.Column(db.Integer, primary_key=True)
+    card_name = db.Column(db.String)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
+
+    user = db.relationship('User', back_populates='credit_cards')
+    store = db.relationship('Store', back_populates='credit_cards')
+
+    def __repr__(self):
+        return f'<CreditCard {self.card_name}>'
 
 
 
--- server/seed.py
+++ server/seed.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 # Standard library imports
-from random import randint, choice as rc
 
+# from random import randint, choice as rc
+import random as rc
 # Remote library imports
 from faker import Faker
 
@@ -20,81 +21,9 @@ if __name__ == '__main__':
 
         print("Starting seed...")
 
-        credit_card_companies = [
-            {
-                "Company": "American Express",
-                "Cards": [
-                    "The Platinum Card from American Express",
-                    "Gold Card from American Express",
-                    "Blue Cash Preferred Card from American Express",
-                    "Delta SkyMiles Credit Cards",
-                    "Hilton Honors Credit Cards",
-                ],
-            },
-            {
-                "Company": "Visa",
-                "Cards": [
-                    "Visa offers a wide range of credit cards, including travel rewards cards, cashback cards, and co-branded cards with various banks.",
-                ],
-            },
-            {
-                "Company": "Mastercard",
-                "Cards": [
-                    "Similar to Visa, Mastercard provides a variety of credit card options, including rewards cards, cashback cards, and co-branded cards.",
-                ],
-            },
-            {
-                "Company": "Discover",
-                "Cards": [
-                    "Discover it Cash Back",
-                    "Discover it Miles",
-                    "Discover it Student Cash Back",
-                    "Discover it Secured",
-                ],
-            },
-            {
-                "Company": "Chase",
-                "Cards": [
-                    "Chase Sapphire Preferred Card",
-                    "Chase Sapphire Reserve",
-                    "Chase Freedom Flex",
-                    "Chase Freedom Unlimited",
-                    "Chase Ink Business Preferred",
-                ],
-            },
-            {
-                "Company": "Bank of America",
-                "Cards": [
-                    "Bank of America Cash Rewards Credit Card",
-                    "Bank of America Travel Rewards Credit Card",
-                    "BankAmericard Credit Card",
-                    "Alaska Airlines Visa Signature Card",
-                ],
-            },
-            {
-                "Company": "Citi",
-                "Cards": [
-                    "Citi Double Cash Card",
-                    "Citi Premier Card",
-                    "Citi Rewards+ Card",
-                    "Costco Anywhere Visa Card by Citi",
-                ],
-            },
-            {
-                "Company": "Capital One",
-                "Cards": [
-                    "Capital One Venture Rewards Credit Card",
-                    "Capital One Quicksilver Cash Rewards Credit Card",
-                    "Capital One Savor Cash Rewards Credit Card",
-                    "Capital One Spark Business Cards (for small businesses)",
-                ],
-            },
-        ]
-
-
         users=[]
         emails=[]
-        for i in range(10):
+        for i in range(20):
             email = fake.email()
             while email in emails:
                 emails = fake.email()
@@ -102,43 +31,65 @@ if __name__ == '__main__':
             user = User(email = email,
                         )
             user.password_hash = user.email + 'password'
-            users.append(user)
             db.session.add(user)
-            db.seesion.commit()
+            db.session.commit()
+            users.append(user)
+         
         
         stores=[]
         discounts = ['5%', '10%', '15%', '20%', '25%', '30%', '40%', '50%', '$5 off', '$10 off', '$15 off', '$20 off', '$25 off', 'BOGO']
         for i in range(20):
-            store_name = fake.commerce.unique.name()
+            store_name = fake.unique.company()
+            expire_date = fake.date()
             store = Store(
                 store_name=store_name,
                 discount=rc.choice(discounts),
+                expire_date=expire_date
             )
-            stores.append(store)
             db.session.add(store)
             db.session.commit()
-
-
+            stores.append(store)
+            
+        
         credit_cards=[]
-        for user in users:
+        credit_card_list = [
+            "The Platinum Card from American Express","Gold Card from American Express",
+            "Blue Cash Preferred Card from American Express","Discover it Cash Back",
+            "Discover it Miles",
+            "Discover it Student Cash Back",
+            "Chase Sapphire Preferred Card",
+            "Chase Sapphire Reserve",
+            "Chase Freedom Flex",
+            "Chase Freedom Unlimited",
+            "Chase Ink Business Preferred",
+            "Bank of America Cash Rewards Credit Card",
+            "BankAmericard Credit Card",
+            "Alaska Airlines Visa Signature Card",
+            "Citi Premier Card",
+            "Citi Rewards+ Card",
+            "Costco Anywhere Visa Card by Citi",
+            "Capital One Venture Rewards Credit Card",
+            "Capital One Spark Business Cards (for small businesses)"
+        ]
+        for store in stores:
             for i in range(rc.randint(1,5)):
-                store=rc.choice(stores)
-                if user not in store.users:
-                    store.users.append(user)
-                    db.session.add(store)
-                    db.session.commit()
-
+                user=rc.choice(users)
+                # if store not in user.stores:
+                #     user.stores.append(store)
+                #     db.session.add(user)
+                #     db.session.commit()
+    
                 credit_card = CreditCard(
-                    cc_name=rc.choice([value for key, value in credit_card_companies])
+                    card_name=rc.choice(credit_card_list),
                     user_id=user.id,
                     store_id=store.id
                 )
                 credit_cards.append(credit_card)
 
 
-    db.session.bulk_save_objects(credit_cards)
-    db.session.commit()
-    db.session.close()
+        db.session.bulk_save_objects(credit_cards)
+        db.session.commit()
+        db.session.close()
 
 
         
