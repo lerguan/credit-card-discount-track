ef8fa8c6273fe237d64bbf430869f54a0d889a7e
Leroy G <liguan698@gmail.com>
2023-10-07 16:46:38 -0700

    create new discount form field

--- client/src/pages/NewDiscount.js
+++ client/src/pages/NewDiscount.js
@@ -1,7 +1,83 @@
 import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
-const NewDiscount = () => {
-  return <>NewDiscount</>;
+const NewDiscount = ({ onAddDiscount, onAddCard }) => {
+  const [card_name, setCard_name] = useState("");
+  const [store_name, setStore_name] = useState("");
+  const [discount, setDiscount] = useState("");
+  const [expire_date, setExpire_date] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("/credit_cards", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        card_name: card_name,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((newCard) => onAddCard(newCard));
+
+    fetch("/stores", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        store_name: store_name,
+        expire_date: expire_date,
+        discount: discount,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((newDiscount) => onAddDiscount(newDiscount));
+  };
+
+  return (
+    <div className="new-discount-info">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Input Credit Card Name:
+          <input
+            type="text"
+            name="card_name"
+            placeholder="Credit Card Name"
+            value={card_name}
+            onChange={(e) => setCard_name(e.target.value)}
+          />
+        </label>
+        <label>
+          Input Store Name:
+          <input
+            type="text"
+            name="store_name"
+            placeholder="Store Name"
+            value={store_name}
+            onChange={(e) => setCard_name(e.target.value)}
+          />
+        </label>
+        <label>
+          Input Discount:
+          <input
+            type="text"
+            name="discount"
+            placeholder="Discount"
+            value={discount}
+            onChange={(e) => setDiscount(e.target.value)}
+          />
+        </label>
+        <label>
+          Input Expire Date:
+          <input
+            type="text"
+            name="expire_date"
+            placeholder="Expire Date"
+            value={expire_date}
+            onChange={(e) => setExpire_date(e.target.value)}
+          />
+        </label>
+        <button type="submit">Add New Discount</button>
+      </form>
+    </div>
+  );
 };
 export default NewDiscount;
--- client/src/pages/Stores.js
+++ client/src/pages/Stores.js
@@ -2,6 +2,14 @@ import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const Stores = () => {
-  return <>Store Lists</>;
+  const [store_name, setStore_name] = useState("");
+  const [expire_date, setExpire_date] = useState("");
+  const [discount, setDiscount] = useState("");
+  const [card_name, setCard_name] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    return <>Store Lists</>;
+  };
 };
 export default Stores;
