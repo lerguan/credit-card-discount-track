37c9c096a9f7e82519aa4bb62bb7eb520fe15fe7
Leroy G <liguan698@gmail.com>
2023-10-12 10:59:47 -0700

    correct cardlist page error when there is no credit card for new user

--- client/src/pages/CardList.js
+++ client/src/pages/CardList.js
@@ -3,15 +3,18 @@ import CreditCardCard from "../components/CreditCardCard";
 
 const CardList = ({ userCreditCards, onDeleteCreditCard }) => {
   const credit_cards = userCreditCards;
-  return (
-    <ul className="credit-cards">
-      {credit_cards.map((credit_card) => {
-        return (
-          <CreditCardCard key={credit_card.id} credit_card={credit_card} onDeleteCreditCard={onDeleteCreditCard} />
-        );
-      })}
-    </ul>
-  );
+
+  if (credit_cards) {
+    return (
+      <ul className="credit-cards">
+        {credit_cards.map((credit_card) => {
+          return (
+            <CreditCardCard key={credit_card.id} credit_card={credit_card} onDeleteCreditCard={onDeleteCreditCard} />
+          );
+        })}
+      </ul>
+    );
+  }
 };
 
 export default CardList;
